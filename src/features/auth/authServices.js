bc832bd64b85f75b29c891a40e291706cf0e1739
sujith kumar <eagle.suji@gmail.com>
2025-06-29 18:16:45 +0530

    Added Login and Login Functionality

--- src/features/auth/authServices.js
+++ src/features/auth/authServices.js
@@ -9,6 +9,16 @@ const register = async (userData) => {
   return response.data
 }
 
-const authService = { register }
+const logout = () => localStorage.removeItem('user')
+
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData)
+  if(response.date){
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+  return response.data
+}
+
+const authService = { register, logout, login }
 
 export default authService
